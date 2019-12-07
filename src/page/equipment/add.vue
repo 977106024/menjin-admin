0147b85d673e2e0949ab98322da1d3c3f7acd055
bohong <977106024@qq.com>
2019-12-07 17:46:25 +0800

    add

--- src/page/equipment/add.vue
+++ src/page/equipment/add.vue
@@ -3,7 +3,7 @@
 
 <!--        上-->
         <header>
-            <img src="" alt="">
+            <img src="@/assets/image/addE.png" alt="">
             <div>
                 <span>设备型号</span>
                 <span> > </span>
@@ -11,14 +11,14 @@
         </header>
 
 <!--        中-->
-        <section>
+        <section class="nameAndNumber">
             <div class="name">
-                <p>设备名称</p>
-                <input type="text">
+                <p class="title">设备名称</p>
+                <input type="text" placeholder="填写设备名称">
             </div>
             <div class="number">
-                <p>设备名称</p>
-                <input type="text">
+                <p class="title">设备编号</p>
+                <input type="text" placeholder="填写设备编号">
             </div>
         </section>
 
@@ -36,5 +36,45 @@
 </script>
 
 <style scoped>
+    .add{
+        padding: .6rem;
+        padding-top: 1.02rem;
+        text-align: left;
+    }
+
+    /*上*/
+    .add header{
+        display: flex;
+        align-items: center;
+    }
+    .add header img{
+         height: 1.68rem;
+         width: 1.3rem;
+        margin-left: .7rem;
+        margin-right: 1.04rem;
+    }
+
+    /*中*/
+    section.nameAndNumber{
+        margin-top: 1.17rem;
+    }
+    section.nameAndNumber>div{
+        padding: .49rem .52rem;
+        background: white;
+        margin-bottom: .3rem;
+    }
+    section.nameAndNumber>div input{
+        border: 0;
+        height: .48rem;
+    }
+    section.nameAndNumber>div .title{
+        color: #3B4859;
+        font-size: .32rem;
+        font-weight: bold;
+        padding-bottom: .35rem;
+    }
+    input::-webkit-input-placeholder {
+        color: #ccc;
+    }
 
 </style>
