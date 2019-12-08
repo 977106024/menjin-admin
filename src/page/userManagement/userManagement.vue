3b4a792539e4a483909c6ca50f0edc50903576d0
MissMao123 <2367066073@qq.com>
2019-12-08 15:08:03 +0800

    添加用户完成

--- src/page/userManagement/userManagement.vue
+++ src/page/userManagement/userManagement.vue
@@ -6,7 +6,7 @@
                 <img src="@/assets/image/user_addUser.png" alt="">
                 <p>添加用户</p>
             </div>
-            <img class="add" src="@/assets/image/user_add.png" alt="">
+            <img class="add" @click="addUser" src="@/assets/image/user_add.png" alt="">
         </div>
         <!-- 用户列表 -->
         <ul class="userList">
@@ -62,6 +62,12 @@ export default {
         ],
             
     }),
+    methods:{
+        // 添加用户
+        addUser(){
+            this.$router.push('/addUser')
+        }
+    }
 }
 </script>
 <style lang="css" scoped>   
